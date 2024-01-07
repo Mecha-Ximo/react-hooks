094485b4051486d4034f9b9e376a55d7b464be8f
Ximo <ximo.dev.94@gmail.com>
2024-01-07 16:13:06 +0100

    test: exclude integration tests in unit

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/dist/config';
+import { configDefaults, defineConfig } from 'vitest/dist/config';
 
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    exclude: [...configDefaults.exclude, 'src/tests/integration'],
   },
 });
